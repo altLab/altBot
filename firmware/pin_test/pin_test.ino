d0d039c2e04a4b9c16b3fc2be4acce7796035b33
Joao Alves <jpralves@gmail.com>
2017-01-29 00:13:55 +0000

    correct testpi for NodeMCU

--- firmware/pin_test/pin_test.ino
+++ firmware/pin_test/pin_test.ino
@@ -1,18 +1,21 @@
-
-const int MAX_PIN = 13;
+  
 const int TEST_TIME_MS = 500;
+byte pins[] = {D0,D1,D2,D3,D4,D5,D6,D7,D8,D9,D10};
+const int MAX_PIN = sizeof(pins)-1;
+
+
 
 void setup() {
   for (int pin=0; pin <= MAX_PIN; ++pin) {
-    pinMode (pin, OUTPUT);
+    pinMode (pins[pin], OUTPUT);
   }
 }
 
 void loop() {
   for (int pin = 0; pin <= MAX_PIN; ++pin) {
-    digitalWrite (pin, HIGH);
+    digitalWrite (pins[pin], HIGH);
     delay (TEST_TIME_MS);
-    digitalWrite (pin, LOW);
+    digitalWrite (pins[pin], LOW);
     delay (TEST_TIME_MS);
   }
 }
