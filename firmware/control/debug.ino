9efdaca44ed63ce718f31353fd0334366a11c106
Pedro Ângelo <pangelo@void.io>
2017-02-14 12:27:45 +0000

    Remove unused functions in util and network modules

--- firmware/control/debug.ino
+++ firmware/control/debug.ino
@@ -50,3 +50,10 @@ void DumpESPinfo() {
   DBG_OUTPUT_PORT.println( ( *xyz ).depc );
 }
 
+String GetMacAddress() {
+  uint8_t mac[6];
+    char macStr[18] = {0};
+  WiFi.macAddress(mac);
+    sprintf(macStr, "%02X:%02X:%02X:%02X:%02X:%02X", mac[0],  mac[1], mac[2], mac[3], mac[4], mac[5]);
+    return  String(macStr);
+}
