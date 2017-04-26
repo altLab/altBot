577cd8018736b60ade5708b751df277d20837b5e
Joao Alves <jpralves@gmail.com>
2017-04-26 18:55:42 +0100

    added to javascript motioncontrol
    added to javascript motioncontrol

--- firmware/control/web_assets.h
+++ firmware/control/web_assets.h
@@ -15,6 +15,7 @@ static const String server_root_assets = R"+++++(
 </head>
 <body>
   <div id="block" style="font-size:24pt">
+    <a href="#" onclick='switch_motion();'>switch motion</a><BR/>
     <a href='#' onclick='move("f");'>forward</a><BR/>
     <a href='#' onclick='move("b");'>backwards</a><BR/>
     <a href='#' onclick='move("l");'>left</a><BR/>
@@ -25,6 +26,16 @@ static const String server_root_assets = R"+++++(
 <script type='text/javascript'>
   var lastMove = 0;
   var version = 8;
+  var motion_enabled = true;
+
+  function switch_motion() {
+    motion_enabled = !motion_enabled;
+    if (motion_enabled) {
+      document.getElementById("dmEvent").innerHTML = "Accelerometer OK";
+    } else {
+      document.getElementById("dmEvent").innerHTML = "Accelerometer disabled";
+    }
+  }
 
   function move_car(left, right) {
     var now = Date.now();
@@ -53,42 +64,44 @@ static const String server_root_assets = R"+++++(
   }
 
   function deviceMotionHandler(eventData) {
-    acceleration = eventData.accelerationIncludingGravity;
-    var left = 0;
-    var right = 0;
-    if (Math.abs(acceleration.y) > 1) { // back-/forward
-      var speed = acceleration.y * 100;
-      if (acceleration.y > 0) { // add 300 to decrease dead zone
-        left = Math.min(1023, speed + acceleration.x * 40 + 300);
-        right = Math.min(1023, speed - acceleration.x * 40 + 300);
-      } else {
-        left = Math.max(-1023, speed + acceleration.x * 40 - 300);
-        right = Math.max(-1023, speed - acceleration.x * 40 - 300);
+    if (motion_enabled) {
+      acceleration = eventData.accelerationIncludingGravity;
+      var left = 0;
+      var right = 0;
+      if (Math.abs(acceleration.y) > 1) { // back-/forward
+        var speed = acceleration.y * 100;
+        if (acceleration.y > 0) { // add 300 to decrease dead zone
+          left = Math.min(1023, speed + acceleration.x * 40 + 300);
+          right = Math.min(1023, speed - acceleration.x * 40 + 300);
+        } else {
+          left = Math.max(-1023, speed + acceleration.x * 40 - 300);
+          right = Math.max(-1023, speed - acceleration.x * 40 - 300);
+        }
+      } else if (Math.abs(acceleration.x) > 1) { // circle only
+        var speed = Math.min(1023, Math.abs(acceleration.x) * 100);
+        if (acceleration.x > 0) {
+          left = Math.min(1023, speed + 300);
+          right = Math.max(-1023, -speed - 300); 
+        } else {
+          left = Math.max(-1023, -speed - 300);  
+          right = Math.min(1023, speed + 300);
+        }
       }
-    } else if (Math.abs(acceleration.x) > 1) { // circle only
-      var speed = Math.min(1023, Math.abs(acceleration.x) * 100);
-      if (acceleration.x > 0) {
-        left = Math.min(1023, speed + 300);
-        right = Math.max(-1023, -speed - 300); 
-      } else {
-        left = Math.max(-1023, -speed - 300);  
-        right = Math.min(1023, speed + 300);
+  
+      if (Math.abs(left) > 200 || Math.abs(right) > 200) { // orig. 100,100
+        move_car(left, right);
       }
+      var direction = "stop";
+      // if direction is opposite, change sign of +left and +right
+      var acc_x = Math.round(acceleration.x);
+      var acc_y = Math.round(acceleration.y);
+      var acc_z = Math.round(acceleration.z);
+      var leftD = Math.round(-left);
+      var rightD = Math.round(-right);
+  
+      direction = "[" + acc_x + "," + acc_y + "," + acc_z  + "]<BR/>" + leftD + ", " + rightD + "<BR/>version: " + version; 
+      document.getElementById("vector").innerHTML=direction;
     }
-
-    if (Math.abs(left) > 200 || Math.abs(right) > 200) { // orig. 100,100
-      move_car(left, right);
-    }
-    var direction = "stop";
-    // if direction is opposite, change sign of +left and +right
-    var acc_x = Math.round(acceleration.x);
-    var acc_y = Math.round(acceleration.y);
-    var acc_z = Math.round(acceleration.z);
-    var leftD = Math.round(-left);
-    var rightD = Math.round(-right);
-
-    direction = "[" + acc_x + "," + acc_y + "," + acc_z  + "]<BR/>" + leftD + ", " + rightD + "<BR/>version: " + version; 
-    document.getElementById("vector").innerHTML =direction;
   }
 </script>
 </body>
